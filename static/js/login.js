a62cd92f3915dba781f67063f4dc6fd0b8ed52b0
Kristian Nissen <kristian.nissen@gmail.com>
2016-11-29 18:29:46 +0000

    More UI things

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,53 @@
-function hello () {
-    console.log('hello kitty');
-}
+"use strict";
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
+ */
+var App = (function() {
+    var foo = document.getElementById('form__login'),
+        fields = ['user_mail', 'user_pwd'];
+
+    function _run() {
+        foo.querySelector('[type="submit"]').addEventListener('click', function (e) {
+            e.preventDefault();
+
+            var inValidFields = _validateFields(); // TODO: Rename function
+            if (inValidFields.length > 0) {
+                for (var f in inValidFields) {
+                    var field = inValidFields[f],
+                        css = field.getAttribute('class'),
+                        snackbar = document.querySelector('#snackbar'),
+                        label = document.querySelector('[for="'+ field.getAttribute('id') +'"]');
+
+                    snackbar.MaterialSnackbar.showSnackbar({
+                        'message': 'The field '+ label.innerText +' is required!'
+                    });
+                    break;
+                }
+            } else {
+                //FIXME: Should be more dynamic
+                if (foo.onsubmit) {
+                    foo.onsubmit();
+                } else if (foo.submit) {
+                    foo.submit();
+                }
+            }
+        });
+    }
+
+    function _validateFields() {
+        var valid = [];
+        for (var f in fields) {
+            var field = foo.querySelector('[name="'+ fields[f] +'"]')
+            if (field.hasAttribute('required')) {
+                if (field.hasAttribute('required') && field.value == '') {
+                    valid.push(field);
+                }
+            }
+        }
+        return valid;
+    }
+
+    return {
+        run: _run
+    }
+})();
--- static/js/require.js
+++ static/js/require.js
@@ -1,14 +1,14 @@
 "use strict";
 
-function require ( file, callback ) {
+function require ( file ) {
     var s, r = false, h = document.querySelector( 'head' );
         s = document.createElement( 'script' );
-        s.src = file;
+        s.src = '/static/js/'+ file;
         s.onload = s.onreadystatechange = function() {
-            //console.log( this.readyState ); //uncomment this line to see which ready states are called.
+            // console.log( this.readyState ); //uncomment this line to see which ready states are called.
             if ( !r && (!this.readyState || this.readyState == 'complete') ) {
                 r = true;
-                callback();
+                App.run();
             }
         };
     h.appendChild( s );
